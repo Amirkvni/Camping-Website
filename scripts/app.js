5855517a358a2b9fb39edd4394190c98a270303c
amirhosein keivani <amirhosein8292@gmail.com>
2024-02-27 00:10:06 +0330

    coding navmenu for mobile

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,15 @@
 const navbtn = document.querySelector(".nav__btn");
+const navMenu = document.querySelector(".nav-menu");
 let navOpen = false;
 navbtn.addEventListener("click", () => {
   if (navOpen == true) {
     navbtn.classList.remove("nav__btn--open");
+    navMenu.classList.remove("nav-menu--open");
+
     navOpen = false;
   } else {
     navbtn.classList.add("nav__btn--open");
     navOpen = true;
+    navMenu.classList.add("nav-menu--open");
   }
 });
